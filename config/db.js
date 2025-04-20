2e046285208aaef6ee0bc85fa49fa18cce6eee53
Shaurya Pratap Singh <shauryasinghad01@gmail.com>
2025-04-20 15:47:21 +0530

    added mongodb in env

--- config/db.js
+++ config/db.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://shauryasingh:2411Patna@cluster0.6zqngoq.mongodb.net/dfchecker"
-    )
-    .then(() => {
-      console.log("Database connected.");
-    });
+  await mongoose.connect(process.env.MONGODB).then(() => {
+    console.log("Database connected.");
+  });
 };
